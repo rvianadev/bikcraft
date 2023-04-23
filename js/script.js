6e30bfb98aeb577c57174130973bc435bcacd501
Rodrigo Viana <roviol@gmail.com>
2023-04-23 19:50:56 -0300

    feat: js script in form budget page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("JavaScript working!");
+// Ativar links do menu principal
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  if (window.location.href.includes(link.href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(window.location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
